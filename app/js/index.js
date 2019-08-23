e13b73fb3a86bb0c2e9266185cdfcc8c366c0756
NatureFreshMilk <naturefreshmilk@github.com>
2019-08-23 14:58:12 +0200

    show items

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,50 @@
 
+
+function getTransformedImage(imageStr){
+	if (imageStr.indexOf("^")){
+		return imageStr.split("^")[0];
+	} else {
+		return imageStr;
+	}
+}
+
 function getImage(node){
+	var imgSrc = "textures/unknown_node.png";
 	if (node.inventory_image){
-		return m("img", { src: "textures/" + node.inventory_image });
+		imgSrc = "textures/" + node.inventory_image;
+	} else if (node.tiles && node.tiles.length > 0) {
+		var tile = node.tiles[0];
+		if (node.tiles.length == 6){
+			tile = node.tiles[5];
+		}
+
+		if (typeof(tile) == "string"){
+			imgSrc = "textures/" + tile;
+		} else if (typeof(tile) == "object" && tile.name) {
+			imgSrc = "textures/" + tile.name;
+		}
 	}
-	if (node.tiles && node.tiles.length) {
-		return m("img", { src: "textures/" + node.tiles[0] });
-	}
+
+	return m("img", { src: getTransformedImage(imgSrc) });
 }
 
 m.request("./mtinfo.json")
 .then(info => {
 	console.log(info);
 
-	const rows = Object.keys(info.nodes)
-	.map(nodename => info.nodes[nodename])
+	const list = [];
+
+	Object.keys(info.nodes)
+	.map(name => info.nodes[name])
+	.forEach(node => list.push(node))
+
+	Object.keys(info.items)
+	.filter(name => !info.nodes[name])
+	.map(name => info.items[name])
+	.forEach(item => list.push(item))
+
+
+	const rows = list
 	.filter(node => !(node.groups && node.groups.not_in_creative_inventory == 1))
 	.map(node => {
 		return m("tr", [
