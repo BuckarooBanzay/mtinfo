b551864b942c9db9309a6f7f7c3c652017cac8a4
NatureFreshMilk <naturefreshmilk@github.com>
2019-08-23 13:29:39 +0200

    simple list

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,36 @@
 
+function getImage(node){
+	if (node.inventory_image){
+		return m("img", { src: "textures/" + node.inventory_image });
+	}
+	if (node.tiles && node.tiles.length) {
+		return m("img", { src: "textures/" + node.tiles[0] });
+	}
+}
+
 m.request("./mtinfo.json")
-.then(info => console.log(info))
+.then(info => {
+	console.log(info);
+
+	const rows = Object.keys(info.nodes)
+	.map(nodename => info.nodes[nodename])
+	.filter(node => !(node.groups && node.groups.not_in_creative_inventory == 1))
+	.map(node => {
+		return m("tr", [
+			m("td", getImage(node)),
+			m("td", node.name)
+		]);
+	});
+
+	const table = m("table", [
+		m("thead", [
+			m("th", [
+				m("td", "Image"),
+				m("td", "Name")
+			])
+		]),
+		m("tbody", rows)
+	]);
+
+	m.render(document.getElementById("app"), table);
+})
