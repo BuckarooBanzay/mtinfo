f5687066074bd319950e447cf742a407f8ae3834
NatureFreshMilk <naturefreshmilk@github.com>
2019-12-09 13:48:44 +0100

    mt 5.1

--- app/start-mod.sh
+++ app/start-mod.sh
@@ -6,6 +6,6 @@ chmod 777 -R /tmp/mtinfo
 docker run --rm -it \
 	-v /tmp/mtinfo/:/var/lib/minetest/.minetest \
 	-v $(pwd)/../:/var/lib/minetest/.minetest/worlds/world/worldmods/mtinfo \
-	registry.gitlab.com/minetest/minetest/server:5.0.1
+	registry.gitlab.com/minetest/minetest/server:5.1.0
 
 cp /tmp/mtinfo/worlds/world/mtinfo.json .
