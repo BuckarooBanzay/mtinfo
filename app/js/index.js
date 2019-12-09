c12c854dfe0e34a98a31870c1c768d1567c77c6f
NatureFreshMilk <naturefreshmilk@github.com>
2019-12-09 13:53:12 +0100

    es6 module

--- app/js/index.js
+++ app/js/index.js
@@ -1,32 +1,8 @@
 
 
-function getTransformedImage(imageStr){
-	if (imageStr.indexOf("^")){
-		return imageStr.split("^")[0];
-	} else {
-		return imageStr;
-	}
-}
+import imageresolver from './util/imageresolver.js';
 
-function getImage(node){
-	var imgSrc = "textures/unknown_node.png";
-	if (node.inventory_image){
-		imgSrc = "textures/" + node.inventory_image;
-	} else if (node.tiles && node.tiles.length > 0) {
-		var tile = node.tiles[0];
-		if (node.tiles.length == 6){
-			tile = node.tiles[5];
-		}
 
-		if (typeof(tile) == "string"){
-			imgSrc = "textures/" + tile;
-		} else if (typeof(tile) == "object" && tile.name) {
-			imgSrc = "textures/" + tile.name;
-		}
-	}
-
-	return m("img", { src: getTransformedImage(imgSrc) });
-}
 
 m.request("./mtinfo.json")
 .then(info => {
@@ -48,7 +24,7 @@ m.request("./mtinfo.json")
 	.filter(node => !(node.groups && node.groups.not_in_creative_inventory == 1))
 	.map(node => {
 		return m("tr", [
-			m("td", getImage(node)),
+			m("td", m("img", { src: imageresolver(node) })),
 			m("td", node.name)
 		]);
 	});
---
+++ app/js/util/imageresolver.js
@@ -0,0 +1,29 @@
+
+
+function getTransformedImage(imageStr){
+	if (imageStr.indexOf("^")){
+		return imageStr.split("^")[0];
+	} else {
+		return imageStr;
+	}
+}
+
+export default function(node){
+	var imgSrc = "textures/unknown_node.png";
+	if (node.inventory_image){
+		imgSrc = "textures/" + node.inventory_image;
+	} else if (node.tiles && node.tiles.length > 0) {
+		var tile = node.tiles[0];
+		if (node.tiles.length == 6){
+			tile = node.tiles[5];
+		}
+
+		if (typeof(tile) == "string"){
+			imgSrc = "textures/" + tile;
+		} else if (typeof(tile) == "object" && tile.name) {
+			imgSrc = "textures/" + tile.name;
+		}
+	}
+
+	return getTransformedImage(imgSrc);
+}
