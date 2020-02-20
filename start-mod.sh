705a91032abe9f3ae9aa62efbe6ccf5acf6513b0
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-02-20 12:35:13 +0100

    cleaner docker start

--- start-mod.sh
+++ start-mod.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-#rm -rf /tmp/mtinfo
-mkdir -p /tmp/mtinfo
-chmod 777 -R /tmp/mtinfo
-rm ./mtinfo -rf
+VOL_ID=$(docker volume create)
+CONTAINER_NAME=mtinfo_tmp
 
-docker run --rm -it \
-	-v /tmp/mtinfo/:/var/lib/minetest/.minetest \
-	-v $(pwd)/:/var/lib/minetest/.minetest/worlds/world/worldmods/mtinfo \
+docker run -it --name ${CONTAINER_NAME} \
+	-u root:root \
+	-v ${VOL_ID}:/root/.minetest/worlds/world/mtinfo \
+	-v $(pwd)/:/root/.minetest/worlds/world/worldmods/mtinfo \
 	registry.gitlab.com/minetest/minetest/server:5.1.0
 
-cp -R /tmp/mtinfo/worlds/world/mtinfo .
+
+docker cp ${CONTAINER_NAME}:/root/.minetest/worlds/world/mtinfo mtinfo
+docker rm ${CONTAINER_NAME}
+docker volume rm ${VOL_ID}
+
