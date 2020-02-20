f4d1604c39336fef496942e54167efc4df787c38
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-02-20 15:10:58 +0100

    dump recipes in detail view

--- app/js/components/nodedetail.js
+++ app/js/components/nodedetail.js
@@ -1,6 +1,9 @@
 
 mtinfo.components.NodeDetail = {
   view(vnode){
-    return m("div", JSON.stringify(mtinfo.nodes[vnode.attrs.nodename]) );
+    return m("div", [
+      "Noddef: " + JSON.stringify(mtinfo.nodes[vnode.attrs.nodename]),
+      "Recipes: " + JSON.stringify(mtinfo.recipes[vnode.attrs.nodename])
+    ]);
   }
 };
