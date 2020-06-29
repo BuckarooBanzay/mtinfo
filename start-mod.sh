0f0ef6fda16ca0cc8e7d29024a533fedf55d6c00
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-06-29 13:26:14 +0200

    fix path copying

--- start-mod.sh
+++ start-mod.sh
@@ -9,7 +9,7 @@ docker run -it --name ${CONTAINER_NAME} \
 	-v $(pwd)/:/root/.minetest/worlds/world/worldmods/mtinfo \
 	registry.gitlab.com/minetest/minetest/server:5.2.0
 
-
+rm mtinfo -rf
 docker cp ${CONTAINER_NAME}:/root/.minetest/worlds/world/mtinfo mtinfo
 docker rm ${CONTAINER_NAME}
 docker volume rm ${VOL_ID}
