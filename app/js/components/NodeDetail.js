7a4b14198033e1d9fc170f9448ad511b5debab8d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-06-29 14:06:52 +0200

    threejs wip

--- app/js/components/NodeDetail.js
+++ app/js/components/NodeDetail.js
@@ -1,9 +1,16 @@
 
+import Preview from './preview/Preview.js';
+
 export default {
   view(vnode){
-    return m("div", [
-      "Noddef: " + JSON.stringify(mtinfo.nodes[vnode.attrs.nodename]),
-      "Recipes: " + JSON.stringify(mtinfo.recipes[vnode.attrs.nodename])
-    ]);
+    return [
+      m("div", [
+        "Noddef: " + JSON.stringify(mtinfo.nodes[vnode.attrs.nodename]),
+        "Recipes: " + JSON.stringify(mtinfo.recipes[vnode.attrs.nodename])
+      ]),
+      m(Preview, {
+        nodename: vnode.attrs.nodename
+      })
+    ];
   }
 };
