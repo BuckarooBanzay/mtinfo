c9c29667d97798f5da1f55fbf144d87c30fa2768
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-02 18:09:25 +0100

    images

--- app/js/components/node-info.js
+++ app/js/components/node-info.js
@@ -1,11 +1,24 @@
 Vue.component("node-info", {
   props: ["name"],
+  computed: {
+    previewImage: function(){
+      console.log(this.name);
+      return mtinfo.imageresolver(mtinfo.nodes[this.name]);
+    }
+  },
   template: /*html*/`
     <div>
       <h4>Node info: {{ name }}</h4>
       <pre>
         {{ JSON.stringify(mtinfo.nodes[name]) }}
       </pre>
+      <div class="row">
+        <div class="col-md-6">
+          <img :src="previewImage" style="height: 64px; width: 64px;"/>
+        </div>
+        <div class="col-md-6">
+        </div>
+      </div>
     </div>
   `
 });
@@ -18,12 +31,16 @@ Vue.component("node-list", {
         <thead>
           <tr>
             <th>Mod</th>
+            <th>Image</th>
             <th>Nodename</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="nodename in Object.keys(mtinfo.nodes)">
-            <td>?</td>
+            <td>{{ nodename.substring(0, nodename.indexOf(":")) }}</td>
+            <td>
+              <img :src="mtinfo.imageresolver(mtinfo.nodes[nodename])"/>
+            </td>
             <td>
               <router-link :to="'/nodes/' + nodename">
                 {{ nodename }}
