b8dcbc3bd563f89e51bac2ca981d3d5a374dacf8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-02 17:34:11 +0100

    routing

--- app/js/components/node-info.js
+++ app/js/components/node-info.js
@@ -9,3 +9,29 @@ Vue.component("node-info", {
     </div>
   `
 });
+
+Vue.component("node-list", {
+  template: /*html*/`
+    <div>
+      <h4>Node list</h4>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Mod</th>
+            <th>Nodename</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="nodename in Object.keys(mtinfo.nodes)">
+            <td>?</td>
+            <td>
+              <router-link :to="'/nodes/' + nodename">
+                {{ nodename }}
+              </router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `
+});
--- app/js/components/start-page.js
+++ app/js/components/start-page.js
@@ -2,6 +2,7 @@ Vue.component("start-page", {
   template: /*html*/`
     <div>
       <h4>MTInfo</h4>
+      <router-link to="/nodes">Nodes</router-link>
     </div>
   `
 });
