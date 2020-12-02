83f9344c3e04fc9ef8128c198102ef9f0ebe2286
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-02 16:07:17 +0100

    vue wip

---
+++ app/js/components/app.js
@@ -0,0 +1,8 @@
+Vue.component("app", {
+  template: /*html*/`
+    <div>
+      <nav-bar/>
+      <router-view></router-view>
+    </div>
+  `
+});
--- app/js/route.js
+++
@@ -1,8 +0,0 @@
-
-import Home from './components/Home.js';
-import NodeDetail from './components/NodeDetail.js';
-
-export default {
-	"/": Home,
-	"/detail/:nodename": NodeDetail
-};
