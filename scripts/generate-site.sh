ecd7797c280a0b50eecaaa164adc39fd8d1f1724
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-11 11:36:39 +0100

    check if the page was generated

--- scripts/generate-site.sh
+++ scripts/generate-site.sh
@@ -6,3 +6,7 @@ docker run --rm -i \
   -v $(pwd)/:/root/.minetest/worlds/world/worldmods/mtinfo \
 	-v $(pwd)/output:/root/.minetest/worlds/world/mtinfo \
 	registry.gitlab.com/minetest/minetest/server:5.3.0
+
+test -f $(pwd)/output/index.html || exit -1
+test -f $(pwd)/output/data/items.json || exit -1
+test -d $(pwd)/output/textures || exit -1
