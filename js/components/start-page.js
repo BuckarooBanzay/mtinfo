7b5236d0db3a71d6da1b4226ab2150d296502979
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-25 12:40:59 +0000

    deploy: 71a29d7048f775dacff6772ef9d3d5f81040b3b6

--- js/components/start-page.js
+++ js/components/start-page.js
@@ -2,8 +2,8 @@ Vue.component("start-page", {
   template: /*html*/`
     <div>
       <h4>MTInfo</h4>
-			<router-link to="/items" class="btn btn-sm btn-primary">
-				<i class="fa fa-question"></i> Items
+			<router-link to="/mods" class="btn btn-sm btn-primary">
+				<i class="fa fa-question"></i> Mods
 			</router-link>
 			<stats/>
     </div>
